5173f2d6a95bc4b6d39a1ad91fc8fcb9c4a70f44
PathNotFound404 <c.pinkston2004@gmail.com>
2026-03-05 15:36:34 -0600

    make car background

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <script setup></script>
 
 <template>
-  <div class="name">
-    <h1>Cody Pinkston</h1>
-    <h4>Full Stack Developer</h4>
-    <div class="brand-icons">
-      <div class="icon-wrapper">  
-        <a href="Pinkston_Resume.pdf" target="_blank"><i class="fa-solid fa-file-lines"></i></a>
-        <span class="icon-text">Resume</span>
+  <div class="hero">
+    <div class="name">
+      <h1>Cody Pinkston</h1>
+      <h4>Full Stack Developer</h4>
+      <div class="brand-icons">
+        <div class="icon-wrapper">  
+          <a href="Pinkston_Resume.pdf" target="_blank"><i class="fa-solid fa-file-lines"></i></a>
+          <span class="icon-text">Resume</span>
+        </div>
+        <div class="icon-wrapper">
+          <a href="https://github.com/PathNotFound404" target="_blank"><i class="fa-brands fa-github"></i></a>
+          <span class="icon-text">Git Hub</span>
+        </div>
+        <div class="icon-wrapper">
+          <a href="https://www.linkedin.com/in/cody-pinkston/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+          <span class="icon-text">LinkedIn</span>
+        </div>
+        <div class="icon-wrapper">
+          <a href="mailto:c.piknston2004@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+          <span class="icon-text">Email</span>
+        </div>
+      </div> 
+      <div class="buttons">
+        <button>About</button>
+        <button>Projects</button>
+        <button>Contact</button>
       </div>
-      <div class="icon-wrapper">
-        <a href="https://github.com/PathNotFound404" target="_blank"><i class="fa-brands fa-github"></i></a>
-        <span class="icon-text">Git Hub</span>
-      </div>
-      <div class="icon-wrapper">
-        <a href="https://www.linkedin.com/in/cody-pinkston/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-        <span class="icon-text">LinkedIn</span>
-      </div>
-    </div> 
-    <div class="buttons">
-      <button>About</button>
-      <button>Projects</button>
-      <button>Contact</button>
     </div>
   </div>
-  
+
+  <div class="about-section">
+    <h2>About Me</h2>
+    <p>
+      I am a full stack developer with a passion for creating dynamic and responsive web applications. With experience in both front-end and back-end development, I enjoy working on projects that challenge my skills and allow me to learn new technologies. I am dedicated to writing clean, efficient code and collaborating with teams to deliver high-quality products.
+    </p>
+  </div>
   
 </template>
 
