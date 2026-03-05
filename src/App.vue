959c04d29718164c9b7a4405c5d4c915283fe835
PathNotFound404 <c.pinkston2004@gmail.com>
2026-03-05 11:07:09 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+
+<template>
+  <div class="name">
+    <h1>Cody Pinkston</h1>
+    <h4>Full Stack Developer</h4>
+    <div class="brand-icons">
+      <div class="icon-wrapper">  
+        <a href="/public/Pinkston_Resume.pdf" target="_blank"><i class="fa-solid fa-file-lines"></i></a>
+        <span class="icon-text">Resume</span>
+      </div>
+      <div class="icon-wrapper">
+        <a href="https://github.com/PathNotFound404" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <span class="icon-text">Git Hub</span>
+      </div>
+      <div class="icon-wrapper">
+        <a href="https://www.linkedin.com/in/cody-pinkston/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+        <span class="icon-text">LinkedIn</span>
+      </div>
+    </div> 
+    <div class="buttons">
+      <button>About</button>
+      <button>Projects</button>
+      <button>Contact</button>
+    </div>
+  </div>
+  
+  
+</template>
+
+<style scoped></style>
